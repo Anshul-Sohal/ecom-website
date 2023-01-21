bbfdc700b0d67033a40532cd082635f0e4ef315c
Anshul-Sohal <anshulsohal.15@gmail.com>
2023-01-21 22:46:46 +0530

    First commit

---
+++ new.js
@@ -0,0 +1,44 @@
+// // 
+// let a=50
+// let b=50
+// console.log("the sum is",a+b)
+// console.log("multiplicationis ",a*b)
+
+
+// let c=a;
+// console.log(c)
+// c+=10
+// console.log(c)
+
+
+// let x=5
+// let y=10
+// console.log(x==y)
+//      console.log(true && true)
+//     console.log(false||false)
+//     console.log(!false)
+
+// function avg(a,b) {
+//     return(a+b)/2
+// }
+// c1=avg(300,500)
+// c2=avg(500,1100)
+// console.log(c1,c2)
+// let age=26
+// if(age>18){
+//     console.log("you are adult")
+// }
+// else if(age>25){
+//     console.log("you are young4")
+// }
+// else if(age>40){
+//     console.log("you are young2")
+// }
+// else {
+//     console.log("you are young1")
+// }
+// let x=12
+// let y=12
+// console.log('the sum is',x+y)
+
+console.log('hello');
